55206221fe045cb3889e802e72925028e0bbbfa4
simoneraeder <166766334+simoneraeder@users.noreply.github.com>
2025-03-12 20:07:17 +0000

    array

--- batalhaNavalNovato.c
+++ batalhaNavalNovato.c
@@ -17,14 +17,14 @@ int main() {
 
     
 
-    int tabuleiro [10] [10];
+    int i, j, tabuleiro [10] [10] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
    
     for (int i = 0; i < 10; i++){
-        for (int j = 0; j < 10; j++)
-      {tabuleiro [i] [j] = i + j;
-        printf("tabuleiro [%d] [%d] = %d\n", i, j, tabuleiro [i] [j]);
+    for (int j = 0; j < 10; j++)
+      {
+        printf(tabuleiro [i] [j], " ");
     }
-    
+    printf("\n");
 }
 
     return 0;
